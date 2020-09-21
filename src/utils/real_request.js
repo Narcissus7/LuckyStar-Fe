00d2dc0e2407978b9176aacdd4c831b5a22e3f91
Narcissus7 <18813104077@163.com>
2020-09-21 14:18:58 +0800

    update

--- src/utils/real_request.js
+++ src/utils/real_request.js
@@ -7,7 +7,8 @@ import { getToken } from '@/utils/auth'
 const service = axios.create({
   // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // baseURL: 'http://127.0.0.1:9002',
-  baseURL: 'http://www.hzmiaow.com:9002',
+  baseURL: 'http://140.143.144.59:9002',
+  // baseURL: 'http://www.hzmiaow.com:9002',
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
